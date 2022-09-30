8b46867bc787bf8f544bdc198d10df47241ec6ad
Kush <kushani96abeysinghe@gmail.com>
2022-09-30 20:24:10 +0530

    modified outlet controller

--- TasteBuds_CW_API/controllers/outletController.js
+++ TasteBuds_CW_API/controllers/outletController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler')
-const Outlet = require("../models/outlet");
+const Outlet = require("../models/oultet");
 
 // @desc     Get Outlets
 // @route    GET /api/outlets
@@ -8,10 +8,72 @@ const getOutlets = asyncHandler(async (req, res) => {
     res.status(200).json(outlets)
 })
 
+// @desc     Get Param Outlets
+// @route    GET /api/outlets/:id
+const getOutletsID = asyncHandler(async(req,res) => {
+    let requestedOutlet = req.params.id;
+    let outlet = await Outlet.findById(requestedOutlet); 
+    if(!outlet){
+        res.status(404)
+        throw new Error('Outlet you are looking for does not exist')
+    }
+    return res.status(200).json(outlet);
+})
+
+// @desc     Add New Outlet
+// @route    POST /api/outlets
+const newOutlet = asyncHandler(async(req,res) =>{
+    if(!req.body.outletID || !req.body.name || !req.body.address || !req.body.picture ){
+        res.status(400)
+        throw new Error("Please fill the required flieds")
+    }else if (req.body.name.length <3 || req.body.address.length <5 ){
+        res.status(400)
+        throw new Error("Please Provide the Correct details.")
+    }
+    let newOutlets = new Outlet ({
+        outletID : req.body.outletID,
+        name : req.body.name,
+        address : req.body.address,
+        rating : req.body.rating,
+        picture : req.body.picture,
+    });
+    try{
+        newOutlets = await newOutlets.save();
+        res.status(200).json(newOutlets);    
+    } catch(ex){
+        res.status(500).send("Error", ex.message);
+    }
+})
+
+// @desc     Remove Specific Outlet
+// @route    DELETE /api/outlets/id
+const deletOutlet = asyncHandler(async(req,res) => {
+    let requestedOutlet = req.params.id;
+    let outletID = await Outlet.find({"outletID" : requestedOutlet})
+    try{
+        let outlet = await Outlet.findByIdAndDelete(outletID); 
+        if(!outlet){
+            return res
+                .status(400)
+                .send("Food you are looking for does not exist")
+        }
+        return res
+            .status(200)
+            .send(`Successfully removed : ${outletID}`);
+    }catch(ex){
+        return res
+            .status(500)
+            .send(ex.message);
+    }
+})
+
 
 
 
 
 module.exports = {
     getOutlets,
+    getOutletsID,
+    newOutlet,
+    deletOutlet
 }
