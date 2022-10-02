ebc3c926682643dc5311b719af2c28e7b646300a
Kush <kushani96abeysinghe@gmail.com>
2022-10-02 07:48:10 +0530

    create user route

--- TasteBuds_CW_API/app.js
+++ TasteBuds_CW_API/app.js
@@ -1,10 +1,12 @@
-const express = require('express');const colors = require('colors')
+const express = require('express');
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const { errorHandler } = require('./middlewares/errorMiddleware')
 const home = require('./routes/home');
 const Outlets = require('./routes/outlets')
 const Foods = require('./routes/foods')
+const user = require('./routes/users')
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,7 +20,7 @@ app.use(express.urlencoded({extended : false}))
 app.use("/",home);
 app.use("/api/outlets", Outlets);
 app.use("/api/foods", Foods);
-
+app.use("/api/users", user);
 
 app.use(errorHandler)
 
