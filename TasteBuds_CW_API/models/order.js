be160f653a01b4aee95b99c4c3bee8314d478dcc
Sachi Perera <sachiper200@gmail.com>
2022-10-04 21:03:27 +0530

    Create order schema
    Add created order schema file (order.js) to repository.

---
+++ TasteBuds_CW_API/models/order.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    Items : [{
+        foodID : {
+            type : mongoose.Schema.Types.ObjectId,
+            require : true,
+            ref : 'Food',
+        },
+    
+        quntity : {
+            type : Number ,
+            require : true,
+        },
+    
+        price : {
+            type : Number,
+            default: 0,
+        },
+    
+        imagePath: {
+            type: String,
+        },
+    }],
+    totalQty: {
+        type: Number,
+        default: 0,
+        required: true,
+      },
+
+      totalCost: {
+        type: Number,
+        default: 0,
+        required: true,
+      },
+
+      user : {
+        type : mongoose.Schema.Types.ObjectId,
+        require : true,
+        ref : 'User',
+    },
+
+      createdDate: {
+        type : Date,
+        default : Date.now
+    },
+});
+
+module.exports = mongoose.model("Order",orderSchema);
