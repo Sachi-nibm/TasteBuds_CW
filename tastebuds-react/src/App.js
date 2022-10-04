6a59cdbbdbe9d73571c800b42d305208b4b763ef
Kush <kushani96abeysinghe@gmail.com>
2022-10-04 18:32:27 +0530

    create signin component

--- tastebuds-react/src/App.js
+++ tastebuds-react/src/App.js
@@ -1,14 +1,16 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Outlets from './pages/outlets';
 import Foods from './pages/foods';
+import SignIn from './components/signIn'
 
 function App() {
     return (
         <div className="App">
         <Router>
             <Routes>
-                <Route path="/outlets" element={<Outlets />} />
+                <Route path="/" element={<Outlets />} />
                 <Route path="/foods" element={<Foods />} />
+                <Route path="/signin" element={<SignIn />} />
             </Routes>
         </Router>
     </div>
---
+++ tastebuds-react/src/components/signIn.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom'
+
+const SignIn =() =>{
+    const [email,setEmail]= React.useState('');
+    const [password,setPassword]= React.useState('');
+    const navigate = useNavigate();
+    const handleLogin= async() => {
+        //console.log(email,password);
+        let result = await fetch("http://localhost:5000/api/signin",{
+            method : 'post',
+            body : JSON.stringify({email, password}),
+            headers: {
+                'content-Type': 'application/json'
+            }
+        });
+        result = await result.json();
+        console.log(result)
+        if(result.email)
+        {
+            localStorage.setItem('user', JSON.stringify(result));
+            localStorage.setItem("isAdmin", (result.isAdmin));
+            console.log('Admin status :',result.isAdmin)
+            localStorage.setItem("login", true);
+            navigate("/")
+        } else {
+            alert("Please enter correct credaintials")
+        }
+    }
+
+    return(
+        <div className='login' style={{marginLeft : "30%" , marginTop : "100px"}}>
+            <h1>Login</h1>
+            <input type="text" placeholder='Enter Email Here' 
+                style={{padding : "7px", display : "block", border : "solid 1px", width : "300px", margin : "20px"}}
+                onChange={(e) => setEmail(e.target.value) } value = {email}
+            />
+            <input type="password" placeholder='Enter Password Here'
+                style={{padding : "7px", display : "block", border : "solid 1px", width : "300px", margin : "20px"}}
+                onChange={(e) => setPassword(e.target.value)} value = {password}
+            />
+            <button onClick={handleLogin} type="button" 
+                style={{margin : "20px", width : "150px", padding : "10px", backgroundColor: "skyblue", border: "solid 1px", cursor: "pointer"}}>
+                Login
+            </button>
+        </div>
+    )
+}
+
+export default SignIn;
