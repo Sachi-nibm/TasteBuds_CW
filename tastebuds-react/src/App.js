b2c1b2dc39fb99f677c2499600e66a3724b94c78
Kush <kushani96abeysinghe@gmail.com>
2022-10-05 22:27:42 +0530

    create sign up component

--- tastebuds-react/src/App.js
+++ tastebuds-react/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Outlets from './pages/outlets';
 import Foods from './pages/foods';
 import SignIn from './components/signIn'
+import SignUp from './components/signUp'
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<Outlets />} />
                 <Route path="/foods" element={<Foods />} />
                 <Route path="/signin" element={<SignIn />} />
+                <Route path="/SignUp" element={<SignUp />} />
             </Routes>
         </Router>
     </div>
