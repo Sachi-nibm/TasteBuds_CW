e6f5631bbdb1c9061475f8882e4fbf95bdf76d16
Sachi Perera <sachiper200@gmail.com>
2022-10-09 21:24:06 +0530

    Create order component
    Create component file of order,jsx file

---
+++ tastebuds-react/src/components/order.jsx
@@ -0,0 +1,37 @@
+// File Name - oredr.jsx
+// File Desc - oredr.jsx
+// Create By - Sachini Perera - 08/10/2022
+import React, { Component } from 'react';
+
+class Order extends Component {
+
+  render() {
+    return (
+      <tr scope="row">
+        <td>
+          <img
+            className="img-fluid"
+            style={{ width: "5rem" }}
+            src={this.props.item.imagePath}
+          />
+        </td>
+        <td>{this.props.item.title}</td>
+        <td>
+          <a href="#" class="border">
+            {this.props.item.qty}
+          </a>
+        </td>
+        <td>
+          Rs. {this.props.item.price} <span className="close"></span>
+        </td>
+        <td>
+          <button className="btn btn-danger" onClick={this.props.onDelete}>
+            Remove
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default Order;
